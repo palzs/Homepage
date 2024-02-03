c7bc22a12b541d9ee9fd0f8eed0b254875ddbec5
palzs <37971999+palzs@users.noreply.github.com>
2024-02-03 13:58:56 +0800

    1

--- scripts.js
+++ scripts.js
@@ -1,23 +1,9 @@
-// 随机句子
-var a = Math.random() + ""
-var rand1 = a.charAt(5)
-quotes = new Array
-quotes[1] = "All those moments will be lost in time,like tears in rain."
-quotes[2] = "Der Gott ist tot."
-quotes[3] = "Veni Vidi Vici."
-quotes[4] = "Cogito, ergo sum."
-quotes[5] = "Je pense, donc je suis."
-quotes[6] = "C'est la vie."
-quotes[7] = "L'enfer, c'est les autres."
-quotes[8] = "Was macht mich nicht umbringt,macht mich stärker."
-quotes[9] = "God's in his heaven, all's right with the world."
-quotes[0] = "Wenn du lange in einen Abgrund blickst,blickt der Abgrund auch in dich hinein."
-var quote = quotes[rand1]
-$(".end").html(quote);
-
-// 搜索脚本
-window.onload = function defaultsearch(){engine = "https://www.baidu.com/s?wd=";}
-$(".searchicon").click(function(){
+// 搜索功能
+window.onload = function defaultsearch()
+    {engine = "https://www.baidu.com/s?wd=";
+    document.querySelector('.baidu-icon').style.filter = "drop-shadow(0 0 2px #aaaaaa)";
+    }
+$(".s-icon").click(function(){
     var key = $(".keyword").val();
     window.open(engine+key);
 });
@@ -27,25 +13,38 @@ if (event.keyCode == 13){
     window.open(engine+key);
     }
 })
-
-// 切换谷歌
-$(".googleicon").click(function(){
+// 引擎切换
+$(".google-icon").click(function(){
     engine = "https://www.google.com/search?q=";
-    document.querySelector('.googleicon').style.filter = "drop-shadow(0 0 2px #aaaaaa)";
-    document.querySelector('.baiduicon').style.filter = "";
-    document.querySelector('.bingicon').style.filter = "";
+    document.querySelector('.google-icon').style.filter = "drop-shadow(0 0 2px #aaaaaa)";
+    document.querySelector('.baidu-icon').style.filter = "";
+    document.querySelector('.bing-icon').style.filter = "";
     });
-// 切换百度
-$(".baiduicon").click(function(){
+$(".baidu-icon").click(function(){
     engine = "https://www.baidu.com/s?wd=";
-    document.querySelector('.googleicon').style.filter = "";
-    document.querySelector('.baiduicon').style.filter = "drop-shadow(0 0 2px #aaaaaa)";
-    document.querySelector('.bingicon').style.filter = "";
+    document.querySelector('.google-icon').style.filter = "";
+    document.querySelector('.baidu-icon').style.filter = "drop-shadow(0 0 2px #aaaaaa)";
+    document.querySelector('.bing-icon').style.filter = "";
     });
-// 切换必应
-$(".bingicon").click(function(){
+$(".bing-icon").click(function(){
     engine = "https://www.bing.com/search?q=";
-    document.querySelector('.googleicon').style.filter = "";
-    document.querySelector('.baiduicon').style.filter = "";
-    document.querySelector('.bingicon').style.filter = "drop-shadow(0 0 2px #aaaaaa)";
+    document.querySelector('.google-icon').style.filter = "";
+    document.querySelector('.baidu-icon').style.filter = "";
+    document.querySelector('.bing-icon').style.filter = "drop-shadow(0 0 2px #aaaaaa)";
     });
+// 随机句子
+var a = Math.random() + ""
+var rand1 = a.charAt(5)
+    quotes = new Array
+    quotes[1] = "All those moments will be lost in time,like tears in rain."
+    quotes[2] = "Der Gott ist tot."
+    quotes[3] = "Veni Vidi Vici."
+    quotes[4] = "Cogito, ergo sum."
+    quotes[5] = "Je pense, donc je suis."
+    quotes[6] = "C'est la vie."
+    quotes[7] = "L'enfer, c'est les autres."
+    quotes[8] = "Was macht mich nicht umbringt,macht mich stärker."
+    quotes[9] = "God's in his heaven, all's right with the world."
+    quotes[0] = "Wenn du lange in einen Abgrund blickst,blickt der Abgrund auch in dich hinein."
+var quote = quotes[rand1]
+$(".end").html(quote);
