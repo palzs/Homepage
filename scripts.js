b7f541ac6eae11a917faba8c93c854aa0cff1c07
palzs <37971999+palzs@users.noreply.github.com>
2024-06-03 13:32:12 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -64,4 +64,3 @@ document.querySelector('.toggle').onclick = function () {
     navigation.classList.toggle('active');
 }
 // 面板赋值
-$(".fav").attr("src","https://raindrop.io/617985487/bookmarks-16689664/embed/hide=header%2Cadd");
